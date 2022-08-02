3f7c700857244f2daf6d28c282a99849f4ae2b7f
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-02 13:26:22 +0100

    Points pairings where brackets are all even

--- Swiss/Pairer.cs
+++ Swiss/Pairer.cs
@@ -12,11 +12,48 @@ public class Pairer
         {
             games = RandomPairing(players);
         }
+        else
+        {
+            games = PointsPairing(players);
+        }
 
         return games;
     }
 
-    private static List<Game> RandomPairing(List<Player> players)
+    private List<Game> PointsPairing(List<Player> players)
+    {
+        List<Game> games = new List<Game>();
+
+        IEnumerable<int> pointsList = players.Select(p => p.Points);
+
+        foreach (var points in pointsList)
+        {
+            List<Player> pointsPlayers = players.Where(p => p.Points == points).ToList();
+
+            while (pointsPlayers.Any())
+            {
+                Game newGame = new Game();
+                newGame.Player1 = pointsPlayers.Random();
+                pointsPlayers.Remove(newGame.Player1);
+
+                if (!pointsPlayers.Any())
+                {
+                    newGame.Player2 = new Player() { Name = "BYE" };
+                }
+                else
+                {
+                    newGame.Player2 = pointsPlayers.Random();
+                    pointsPlayers.Remove(newGame.Player2);
+                }
+
+                games.Add(newGame);
+            }
+        }
+
+        return games;   
+    }
+
+    private List<Game> RandomPairing(List<Player> players)
     {
         List<Game> games = new List<Game>();
 
