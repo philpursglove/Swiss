88b9fa95936a951bd57c7c838ff5b3c823e2a576
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-05 23:22:16 +0100

    I did a Fluent

--- Swiss/Player.cs
+++ Swiss/Player.cs
@@ -1,4 +1,7 @@
-﻿namespace Swiss;
+﻿using System;
+using System.Xml.Linq;
+
+namespace Swiss;
 
 public class Player
 {
@@ -9,5 +12,38 @@ public class Player
     public int Points { get; set; }
 
     public bool Bye { get; set; }
+}
 
+public class PlayerBuilder
+{
+    Player _player = new Player();
+
+    public PlayerBuilder WithName(string name)
+    {
+        _player.Name = name;
+        return this;
+    }
+
+    public PlayerBuilder WithDropped(bool dropped)
+    {
+        _player.Dropped = dropped;
+        return this;
+    }
+
+    public PlayerBuilder WithPoints(int points)
+    {
+        _player.Points = points;
+        return this;
+    }
+
+    public PlayerBuilder WithBye(bool bye)
+    {
+        _player.Bye = bye;
+        return this;
+    }
+
+    public Player Build()
+    {
+        return _player;
+    }
 }
