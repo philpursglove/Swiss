38de2f62334b868697f26ef2190dc7ccb90fe148
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-06 19:49:21 +0100

    RF PlayerBuilder out to separate file

--- Swiss/Player.cs
+++ Swiss/Player.cs
@@ -13,37 +13,3 @@ public class Player
 
     public bool Bye { get; set; }
 }
-
-public class PlayerBuilder
-{
-    Player _player = new Player();
-
-    public PlayerBuilder WithName(string name)
-    {
-        _player.Name = name;
-        return this;
-    }
-
-    public PlayerBuilder WithDropped(bool dropped)
-    {
-        _player.Dropped = dropped;
-        return this;
-    }
-
-    public PlayerBuilder WithPoints(int points)
-    {
-        _player.Points = points;
-        return this;
-    }
-
-    public PlayerBuilder WithBye(bool bye)
-    {
-        _player.Bye = bye;
-        return this;
-    }
-
-    public Player Build()
-    {
-        return _player;
-    }
-}
---
+++ Swiss/PlayerBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace Swiss;
+
+public class PlayerBuilder
+{
+    Player _player = new Player();
+
+    public PlayerBuilder WithName(string name)
+    {
+        _player.Name = name;
+        return this;
+    }
+
+    public PlayerBuilder WithDropped(bool dropped)
+    {
+        _player.Dropped = dropped;
+        return this;
+    }
+
+    public PlayerBuilder WithPoints(int points)
+    {
+        _player.Points = points;
+        return this;
+    }
+
+    public PlayerBuilder WithBye(bool bye)
+    {
+        _player.Bye = bye;
+        return this;
+    }
+
+    public Player Build()
+    {
+        return _player;
+    }
+}
